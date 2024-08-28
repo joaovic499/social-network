0e540fa9c43b5f6c5c3619ff09b071530b0d3297
joaovic499 <joaovictorsilva60@live.com>
2024-08-27 22:23:27 -0300

    Refine Register controller for register a user

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,22 @@
-require "application_controller"
-
 class LoginController < ApplicationController
-  def index
+  protect_from_forgery with: :null_session
+
+  def home
+    redirect_to @home
+  end
+
+  def register
+    @login = Login.new(login_params)
+
+    if @login.save
+      render :home
+    else 
+      render :home, status: :unprocessable_entity
+    end
+end
+
+  private def login_params
+
+    params.require(:user).permit(:email, :name, :password)
   end
 end
--- app/models/login.rb
+++ app/models/login.rb
@@ -1,4 +1,8 @@
 class Login < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 10 }
+    validates :name, presence: true, length: { minimum: 10 }
+
     def change
         create_table :login do |l|
             t.string :email
